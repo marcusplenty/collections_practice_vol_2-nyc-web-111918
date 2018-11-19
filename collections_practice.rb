d9269ae9142e124397044221ef07e407ab610f71
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 07:39:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,80 @@
-# your code goes here
+def begins_with_r(array)
+	array.all?{|word| word[0] == "r"}	
+end
+
+def contain_a(array)
+  x = array.select{|word| word.include?("a")}
+  return x
+end
+
+def first_wa(array)
+  x=array.select{|word| word.class == String && word.include?("wa")}
+  return x[0]
+end
+
+def remove_non_strings(array)
+  array.reject{|ele| ele.class != String}
+end
+
+def count_elements(array)
+  y = {}
+  array.each do |element|
+    if y.has_key?(element[:name])
+      y[element[:name]] +=1
+    else
+      y[element[:name]] = 1
+    end
+  end
+  x = []
+  y.each do |key,value|
+      z = {:name => key, :count => value}
+      x << z
+  end
+  return x
+end
+
+def merge_data(keys,data)
+  arr1 = []
+  i = 0
+  keys.each do |hash1| 
+    arr1 << hash1
+    data.each do |hash2| #array index 0,1
+        hash2.each do |k1,v1| #blake -> {}
+          if k1 == hash1[:first_name] 
+            v1.each do |k2,v2| #awesomes -> values
+              arr1[i][k2] = v2
+            end
+          end
+        end
+      
+    end
+    i+=1
+  end
+end
+
+def find_cool(array)
+  x = []
+  array.each do |element| # index
+    element.each do |keys,values|
+      if element[keys] == "cool"
+        x << element
+        return x
+      end 
+    end 
+  end
+end
+
+def organize_schools(hash)
+  hash2 = {}
+  hash.each do |k1,v1| # school name -> hash of location
+    v1.each do |k2,v2| # location -> city
+      if hash2.has_key?(v2)
+         hash2[v2] << k1
+      else
+        hash2[v2] = [k1]
+      end
+    end
+  end
+  return hash2
+  
+end 
